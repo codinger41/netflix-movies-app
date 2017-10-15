75c9285b0f99bb87c910763247341bca2905b841
leksyib <leksyib14@gmail.com>
2017-10-15 16:17:37 +0100

    added code

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { DrawerNavigator, DrawerItems } from 'react-navigation';
+import HomeStackNavigator from './src/components/navigation/home-stack-navigator';
+import { COLORS } from './src/constants/styles';
+import styled from 'styled-components/native';
+
+const DrawerContainer = styled.View`
+  flex: 1;
+  background-color: ${COLORS.GREY.BRIGHT_GREY};
+`;
+
+const AppContainer = styled.View`
+  flex: 1;
+  background-color: ${COLORS.GREY.BLACK_RUSSIAN};
+`;
+
+const drawerRouteConfig = {
+  Home: {
+    screen: HomeStackNavigator,
+  },
+};
+
+const CustomDrawerContentComponent = props => (
+  <DrawerContainer>
+    <DrawerItems {...props} />
+  </DrawerContainer>
+);
+
+const drawerNavigatorConfig = {
+  contentComponent: props => <CustomDrawerContentComponent {...props} />,
+};
+
+const AppDrawer = DrawerNavigator(drawerRouteConfig, drawerNavigatorConfig);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppContainer>
+        <StatusBar hidden={true} />
+        <AppDrawer />
+      </AppContainer>
+    );
+  }
+}
---
+++ src/components/navigation/home-stack-navigator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import HomeScreen from '../../components/screens/home-screen';
+import ShowDetailsScreen from '../../components/screens/show-details-screen';
+
+const HomeStackNavigator = StackNavigator(
+  {
+    Main: { screen: HomeScreen },
+    ShowDetails: { screen: ShowDetailsScreen },
+  },
+  {
+    initialRouteName: 'Main',
+    headerMode: 'none',
+  },
+);
+
+export default HomeStackNavigator;
